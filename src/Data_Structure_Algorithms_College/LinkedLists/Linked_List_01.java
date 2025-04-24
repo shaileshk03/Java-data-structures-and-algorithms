c2b3793434eb565e1dcff5d7e3d31993ce42371d
Shailesh <shaileshksingh4@gmail.com>
2025-04-24 09:56:00 +0530

    Added remove first method in LinkedList

--- src/Data_Structure_Algorithms_College/LinkedLists/Linked_List_01.java
+++ src/Data_Structure_Algorithms_College/LinkedLists/Linked_List_01.java
@@ -1,9 +1,13 @@
 package Data_Structure_Algorithms_College.LinkedLists;
+
+import java.awt.*;
+
 /**
  * @author Shailesh
  */
-public class LinkedList01 {
-  public static class Node{
+public class Linked_List_01 {
+
+    public static class Node{
       int data;
       Node next;
 
@@ -16,12 +20,13 @@ public class LinkedList01 {
 
    public static Node head;
    public static Node tail;
+   public static int size;
 
    // Add
    public void addFirst(int data){
-
        // step1 create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null){
             head = tail = newNode;
             return;
@@ -37,7 +42,7 @@ public class LinkedList01 {
    public void addLast(int data){
        // step1 create new node
        Node newNode = new Node(data);
-
+       size++;
        if (head == null){
            head = tail = newNode;
            return;
@@ -57,21 +62,65 @@ public class LinkedList01 {
 
        Node temp = head;
        while (temp != null){
-           System.out.println(temp.data+ "");
+           System.out.print(temp.data + "->");
            temp = temp.next;
        }
        System.out.println();
    }
 
+   // Add
+    public void add(int idx, int data){
+       // if operation is to add List in first
+       if(idx == 0){
+           addFirst(data);
+           return;
+       }
+
+      Node newNode = new Node(data);
+       size++;
+      Node temp = head;
+      int i = 0;
+
+      while (i < idx-1){
+          temp = temp.next;
+          i++;
+      }
+
+      // i = idx -1; temp -> prev
+     newNode.next = temp.next;
+      temp.next = newNode;
+    }
+
+    // Remove first
+    public int removeFirst(){
+       // base case
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size ==1) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+
+        int val = head.data;
+       head = head.next;
+       return val;
+    }
+
+
     public static void main(String[] args) {
-        LinkedList01 ls = new LinkedList01();
-        ls.print();
+        Linked_List_01 ls = new Linked_List_01();
         ls.addFirst(2);
-        ls.print();
         ls.addFirst(1);
-        ls.print();
         ls.addLast(3);
-        ls.print();
         ls.addLast(4);
+        ls.add(2, 3);
+
+        ls.print();
+        System.out.println("Size of LinkedList is : " + ls.size);
+
+        ls.removeFirst();
+        ls.print();
     }
 }
---
+++ src/Data_Structure_Algorithms_College/LinkedLists/Linked_List_02.java
@@ -0,0 +1,9 @@
+package Data_Structure_Algorithms_College.LinkedLists;
+
+/**
+ * @author Shailesh
+ */
+public class Linked_List_02 {
+
+
+}
